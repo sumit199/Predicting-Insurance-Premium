efed1a0887cbc2003cab490af92cfd3da6bd9d95
sumit199 <bhadouriasumit27@gmail.com>
2023-03-10 22:38:49 +0530

    model evaluation save

--- insurance/component/model_evaluation.py
+++ insurance/component/model_evaluation.py
@@ -28,6 +28,64 @@ class ModelEvaluation:
     def initiate_model_evaluation(self):
 
         try:
-            pass
+            #if saved model folder has model the we will compare 
+            #which model is best trained or the model from saved model folder
+
+            logging.info("if saved model folder has model the we will compare "
+            "which model is best trained or the model from saved model folder")
+            latest_dir_path = self.model_resolver.get_latest_dir_path()
+            if latest_dir_path==None:
+                model_eval_artifact = artifact_entity.ModelEvaluationArtifact(is_model_accepted=True,
+                improved_accuracy=None)
+                logging.info(f"Model evaluation artifact: {model_eval_artifact}")
+                return model_eval_artifact
+
+            #Finding location of transformer model and target encoder
+            logging.info("Finding location of transformer model and target encoder")
+            transformer_path = self.model_resolver.get_latest_transformer_path()
+            model_path = self.model_resolver.get_latest_model_path()
+            target_encoder_path = self.model_resolver.get_latest_target_encoder_path()
+
+            logging.info("Previous trained objects of transformer, model and target encoder")
+            #Previous trained  objects
+            transformer = load_object(file_path=transformer_path)
+            model = load_object(file_path=model_path)
+            target_encoder = load_object(file_path=target_encoder_path)
+            
+
+            logging.info("Currently trained model objects")
+            #Currently trained model objects
+            current_transformer = load_object(file_path=self.data_transformation_artifact.transform_object_path)
+            current_model  = load_object(file_path=self.model_trainer_artifact.model_path)
+            current_target_encoder = load_object(file_path=self.data_transformation_artifact.target_encoder_path)
+
+            test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
+            y_true = test_df[TARGET_COLUMN]
+            input_feature_test_df=test_df.drop(TARGET_COLUMN,axis=1)
+            input_feature_test_df = target_encoder(df=input_feature_test_df, 
+                                CATEGORICAL_COLUMN=CATEGORICAL_COLUMN)
+
+            # accuracy using previous trained model
+            input_arr =transformer.transform(input_feature_test_df)
+            y_pred = model.predict(input_arr)
+            print(f"Prediction using previous model: {y_pred[:5]}")
+            previous_model_score = f1_score(y_true=y_true, y_pred=y_pred)
+            logging.info(f"Accuracy using previous trained model: {previous_model_score}")
+
+            # accuracy using current trained model
+            input_arr =current_transformer.transform(input_feature_test_df)
+            y_pred = current_model.predict(input_arr)
+            y_true =test_df[TARGET_COLUMN]
+            print(f"Prediction using trained model: {y_pred[:5]}")
+            current_model_score = f1_score(y_true=y_true, y_pred=y_pred)
+            logging.info(f"Accuracy using current trained model: {current_model_score}")
+            if current_model_score<=previous_model_score:
+                logging.info(f"Current trained model is not better than previous model")
+                raise Exception("Current trained model is not better than previous model")
+
+            model_eval_artifact = artifact_entity.ModelEvaluationArtifact(is_model_accepted=True,
+            improved_accuracy=current_model_score-previous_model_score)
+            logging.info(f"Model eval artifact: {model_eval_artifact}")
+            return model_eval_artifact
         except Exception as e:
             raise InsuranceException(e, sys)
