ad1aa11a8c98a0ce68cf5e58a3403098199f9bfe
sumit199 <bhadouriasumit27@gmail.com>
2023-03-11 18:37:39 +0530

    save model pusher

--- insurance/component/model_pusher.py
+++ insurance/component/model_pusher.py
@@ -2,8 +2,7 @@ from insurance.predictor import ModelResolver
 from insurance.entity import config_entity,artifact_entity
 from insurance.exception import InsuranceException
 from insurance.logger import logging
-from insurance.utils import load_object
-import pandas  as pd
+from insurance.utils import save_object, load_object
 import sys,os
 
 
@@ -24,7 +23,33 @@ class ModelPusher:
     
     def initiate_model_pusher(self,)->ModelPusherArtifact:
         try:
-            pass
-        
+             #load object
+            logging.info(f"Loading transformer model and target encoder")
+            transformer = load_object(file_path=self.data_transformation_artifact.transform_object_path)
+            model = load_object(file_path=self.model_trainer_artifact.model_path)
+            target_encoder = load_object(file_path=self.data_transformation_artifact.target_encoder_path)
+
+            #model pusher dir
+            logging.info(f"Saving model into model pusher directory")
+            save_object(file_path=self.model_pusher_config.pusher_transformer_path, obj=transformer)
+            save_object(file_path=self.model_pusher_config.pusher_model_path, obj=model)
+            save_object(file_path=self.model_pusher_config.pusher_target_encoder_path, obj=target_encoder)
+
+
+            #saved model dir
+            logging.info(f"Saving model in saved model dir")
+            transformer_path=self.model_resolver.get_latest_save_transformer_path()
+            model_path=self.model_resolver.get_latest_save_model_path()
+            target_encoder_path=self.model_resolver.get_latest_save_target_encoder_path()
+
+            save_object(file_path=transformer_path, obj=transformer)
+            save_object(file_path=model_path, obj=model)
+            save_object(file_path=target_encoder_path, obj=target_encoder)
+
+            model_pusher_artifact = ModelPusherArtifact(pusher_model_dir=self.model_pusher_config.pusher_model_dir,
+                                                        saved_model_dir=self.model_pusher_config.saved_model_dir)
+            logging.info(f"Model pusher artifact: {model_pusher_artifact}")
+            return model_pusher_artifact
+
         except Exception as e:
             raise InsuranceException(e, sys)
